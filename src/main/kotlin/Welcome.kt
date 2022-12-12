3ece04d22c1ac8673027ffc97f6ca96205da1325
Vadim <herrschmann@gmail.com>
2022-12-12 20:20:17 +0500

    up

--- src/main/kotlin/Welcome.kt
+++ src/main/kotlin/Welcome.kt
@@ -1,19 +1,17 @@
-import csstype.px
-import csstype.rgb
+import csstype.ClassName
 import data.Manager
 import data.Zakaz
-import react.FC
-import react.Props
-import emotion.react.css
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import react.dom.html.InputType
-import react.dom.html.ReactHTML.div
-import react.dom.html.ReactHTML.input
+import react.FC
+import react.Props
+import react.dom.html.ReactHTML.h3
+import react.dom.html.ReactHTML.img
 import react.dom.html.ReactHTML.li
 import react.dom.html.ReactHTML.ul
 import react.useEffectOnce
 import react.useState
+
 
 external interface WelcomeProps : Props {
 
@@ -23,7 +21,7 @@ val mainScope = MainScope()
 
 
 val Welcome = FC<WelcomeProps> { props ->
-    var orders  : List<Zakaz> by useState(emptyList())
+    var orders: List<Zakaz> by useState(emptyList())
 
     useEffectOnce {
         mainScope.launch {
@@ -38,14 +36,21 @@ val Welcome = FC<WelcomeProps> { props ->
         orders.forEach {
 
             li {
-                css {
-                    padding = 5.px
-                    backgroundColor = rgb(8, 97, 22)
-                    color = rgb(56, 246, 137)
+                img {
+                    src = "11.jpg"
+                    alt = "ХЗХЗ"
+                    className = ClassName("project__img")
                 }
-                +"${it.phone}"
-                +"${it.fullName}"
+
+                h3 {
+                    +"${it.phone}"
+                    +"${it.fullName}"
+                    className = ClassName("project__title")
+                }
+
+                className = ClassName("project")
             }
+
         }
     }
 
